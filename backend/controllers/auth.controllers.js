59f39633b42fe01ae13654fccc039db77f43b52c
Ekansh-2004 <chauhan.ekansh06@gmail.com>
2025-06-20 23:03:21 +0530

    login logout func added

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -58,6 +58,51 @@ export const signup = async (req, res) => {
 		res.status(500).json({ error: "Internal Server Error" });
 	}
 };
-export const login = async (req, res) => {};
-export const logout = async (req, res) => {};
-export const getMe = async (req, res) => {};
+
+// LOGIN IN FUNCTION
+export const login = async (req, res) => {
+	try {
+		const { fullName, password } = req.body;
+
+		const user = await User.findOne({ fullName });
+
+		if (!user) {
+			res.status(400).json({ error: "Invalid fullName" });
+		}
+		//check whether the password submitted is equal to the password in the db
+		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+		if (!isPasswordCorrect) {
+			res.status(400).json({ error: "Invalid Password" });
+		}
+
+		generateTokenAndSetCookie(user._id, res);
+		res.status(201).json({
+			_id: user._id,
+			fullName: user.fullName,
+		});
+	} catch (error) {
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
+// LOG OUT FUNCTION
+export const logout = async (req, res) => {
+	try {
+		res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: "Logout Out successfully" });
+	} catch (error) {
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
+// GET ME
+export const getMe = async (req, res) => {
+	try {
+		const user = await User.findById(req.user._id).select("-password");
+
+		res.status(200).json(user);
+	} catch (error) {
+		console.error("Auth error:", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
--- backend/routers/auth.routes.js
+++ backend/routers/auth.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getMe, login, logout, signup } from "../controllers/auth.controllers.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 const router = express.Router();
 
-router.get("/me", getMe);
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
 router.post("/logout", logout);
 router.post("/login", login);
