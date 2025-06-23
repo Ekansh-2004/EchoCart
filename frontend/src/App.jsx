9e3948538961ec0af12b952401a1be1611033d76
Naman Arora <naman22arora05@gmail.com>
2025-06-23 14:29:27 +0530

    Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import Loginpage from "./components/Loginpage";
 import Signup from "./components/Signup";
+import ShoppingCart from "./components/shopping-cart";
 
 function App() {
 	return (
@@ -14,6 +15,10 @@ function App() {
 				<Route
 					path="/signup"
 					element={<Signup />}
+				/>
+				<Route
+					path="/shopping-cart"
+					element={<ShoppingCart />}
 				/>
 			</Routes>
 		</Router>
